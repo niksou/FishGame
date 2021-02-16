c5715024d7a0fb26395e49baef74beb2d6a5c8a7
Nikita Soumrov <niksou1994@gmail.com>
2021-02-16 20:53:52 +0200

    level 2 is all functional and works well

--- Assets/PlayerMovment1.cs
+++ Assets/PlayerMovment1.cs
@@ -16,10 +16,20 @@ public class PlayerMovment1 : MonoBehaviour
 
     private Rigidbody2D rigidBody;
     static int counter = 0;
+
+    GameObject key;
     
     GameObject digit;
     GameObject digit1;
     GameObject digit2;
+
+    GameObject yStar;
+    GameObject rStar;
+    GameObject gStar;
+
+    static int yCount = 0;
+    static int rCount = 0;
+    static int gCount = 0;
 
     
 
@@ -35,6 +45,14 @@ public class PlayerMovment1 : MonoBehaviour
         digit2.SetActive(false);
 
 
+        yStar = GameObject.Find("yellow_seashel");
+        yStar.SetActive(false);
+        rStar = GameObject.Find("pink_star");
+        rStar.SetActive(false);
+        gStar = GameObject.Find("green star");
+        gStar.SetActive(false);
+        key = GameObject.Find("key");
+        key.SetActive(false);
 
     }
 
@@ -50,6 +68,11 @@ public class PlayerMovment1 : MonoBehaviour
         {
             // Scale x to either positive or negative 1 to 'turn' the character
             transform.localScale = new Vector3(Mathf.Sign(deltaX), 1.0f, 1.0f);
+        }
+
+        if (yCount == 5 && rCount == 3 && gCount == 1)
+        {
+            key.SetActive(true);
         }
      
 
@@ -77,10 +100,31 @@ public class PlayerMovment1 : MonoBehaviour
             else
             {
                 Destroy(col.gameObject);
+                yStar.SetActive(true);
+                rStar.SetActive(true);
+                gStar.SetActive(true);
+
             }
         }
-       
-       
+
+
+        if (col.gameObject == yStar && yCount < 5)
+        {
+            yCount++;
+        }
+
+        if (col.gameObject==rStar&&rCount<3)
+        {
+            rCount++;
+        }
+
+        if (col.gameObject == gStar && gCount < 1)
+        {
+            gCount++;
+        }
+        
+        
+
 
     }
 
